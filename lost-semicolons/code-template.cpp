15056936cb1f20dacc46d60cca4e71f1ffe97ff9
Marc Steven <mackyclemen@gmail.com>
2020-10-27 07:32:16 +0800

    Initial commit

---
+++ lost-semicolons/code-template.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+
+    string ACM_Members = "ACMblers";
+
+    cout << "Hey there, " << ACM_Members << endl
+    cout << "Welcome to Rookie Rush!" << endl
+    cout << "Here you will (re)learn the fundamentals of programming!" << endl
+
+    return 0
+}
